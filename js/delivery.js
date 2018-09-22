a0741d2bef3c8386aa3a5ac498c50a20b4e65cde
Alena Kazarina <alenakazarina1@gmail.com>
2018-09-22 13:13:58 +0300

    added delivery IIFE

--- js/delivery.js
+++ js/delivery.js
@@ -1,22 +1,24 @@
 'use strict';
 
-function changeDeliveryMethod(evt) {
-  var target = evt.target;
-  target.checked = true;
-  if (target.id === 'deliver__store') {
-    showDeliverStore();
+(function () {
+  function changeDeliveryMethod(evt) {
+    var target = evt.target;
+    target.checked = true;
+    if (target.id === 'deliver__store') {
+      showDeliverStore();
+    }
+    if (target.id === 'deliver__courier') {
+      showDeliverCourier();
+    }
   }
-  if (target.id === 'deliver__courier') {
-    showDeliverCourier();
+  function showDeliverStore() {
+    document.querySelector('.deliver__courier').classList.add('visually-hidden');
+    document.querySelector('.deliver__store').classList.remove('visually-hidden');
   }
-}
-function showDeliverStore() {
-  document.querySelector('.deliver__courier').classList.add('visually-hidden');
-  document.querySelector('.deliver__store').classList.remove('visually-hidden');
-}
-function showDeliverCourier() {
-  document.querySelector('.deliver__store').classList.add('visually-hidden');
-  document.querySelector('.deliver__courier').classList.remove('visually-hidden');
-}
-var deliveryTabs = document.querySelector('.deliver__toggle');
-deliveryTabs.addEventListener('click', changeDeliveryMethod);
+  function showDeliverCourier() {
+    document.querySelector('.deliver__store').classList.add('visually-hidden');
+    document.querySelector('.deliver__courier').classList.remove('visually-hidden');
+  }
+  var deliveryTabs = document.querySelector('.deliver__toggle');
+  deliveryTabs.addEventListener('click', changeDeliveryMethod);
+})();
